d8c74bab58eec417b234a1d1db4c74f7987c9dd3
AaronSledge <ahern2118@gmail.com>
2024-07-21 04:40:12 -0700

    Design website

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ let quoteText = document.getElementById("quote");
 function fetchQuote() {
     fetch("https://api.quotable.io/random")
     .then(response => response.json())
-    .then(data => quoteText.textContent = data.content + " by " + data.author)
+    .then(data => quoteText.textContent = data.content + "\r\n" + "by " + data.author)
     .catch(error => console.log(error));
 }
